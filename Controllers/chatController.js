302b4747fd06e3b8f6381ceddd29e5b578473b26
IbrahimTaibi <168765185+IbrahimTaibi@users.noreply.github.com>
2024-06-02 14:03:04 +0100

    Update chatController.js

--- Controllers/chatController.js
+++ Controllers/chatController.js
@@ -1,18 +1,13 @@
-const Chat = require("../Models/chatSchema");
+// chatController.js
 const ChatMessage = require("../Models/chatMessage");
+const Chat = require("../Models/chatSchema");
 
 exports.getChatsForUser = async (req, res) => {
   try {
     const { userId } = req.params;
     const chats = await Chat.find({ users: userId })
       .populate("users", "username")
-      .populate({
-        path: "latestMessage",
-        populate: {
-          path: "sender receiver",
-          select: "username",
-        },
-      });
+      .populate("latestMessage");
     res.status(200).json({
       status: "success",
       results: chats.length,
